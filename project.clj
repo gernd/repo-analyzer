7d5c60023b5329cfd1a1c0acb0cf394f2e7c305f
Gernot Pointner <gernot.pointner@googlemail.com>
2019-10-22 15:19:18 +0200

    print logs of given repo

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"],
+                 [clj-jgit "1.0.0-beta2"]
+                 ]
   :main ^:skip-aot repo-analyzer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/repo_analyzer/core.clj
+++ src/repo_analyzer/core.clj
@@ -1,7 +1,18 @@
 (ns repo-analyzer.core
   (:gen-class))
 
+(use 'clj-jgit.porcelain)
+(use 'clojure.pprint)
+
+(defn analyze-repository
+  "Analyzes the given GIT repository"
+  [path-to-repo]
+  (with-repo path-to-repo
+             (pprint (git-log repo))))
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [path-to-repo]
+  (println "Analyzing" path-to-repo)
+  (analyze-repository path-to-repo))
+
