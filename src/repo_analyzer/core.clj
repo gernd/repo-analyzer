f4df0c3e3cc8487efe1ba1c70966b852b953033c
Gernot Pointner <gernot.pointner@googlemail.com>
2019-10-24 14:54:41 +0200

    Get commits by author

--- src/repo_analyzer/core.clj
+++ src/repo_analyzer/core.clj
@@ -3,12 +3,26 @@
 
 (use 'clj-jgit.porcelain)
 (use 'clojure.pprint)
+(use 'clojure.tools.trace)
+
+(defn get-logs-by-author
+  "Creates a map author -> commits from the given logs"
+  [logs]
+  (reduce #(let [author-name (:name (:author %2))]
+             (if (contains? %1 author-name)
+               (assoc %1 author-name (cons (get %1 author-name) %2))
+               (assoc %1 author-name ())))
+          {}
+          logs))
+
 
 (defn analyze-repository
   "Analyzes the given GIT repository and returns the analysis"
   [path-to-repo]
   (with-repo path-to-repo
-             {:logs (git-log repo)}))
+             (let [logs (git-log repo)]
+               {:logs      logs
+                :by-author (get-logs-by-author logs)})))
 
 (defn render-analysis
   "Renders the given analysis"
--- test/repo_analyzer/core_test.clj
+++ test/repo_analyzer/core_test.clj
@@ -1,6 +1,7 @@
 (ns repo-analyzer.core-test
   (:require [clojure.test :refer :all]
-            [repo-analyzer.core :refer :all]))
+            [repo-analyzer.core :refer :all]
+            [clojure.tools.trace :refer :all]))
 
 (deftest test-analyze-repository
   (testing "Tests the analyze-repository function using the project's repository"
