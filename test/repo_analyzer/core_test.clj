7812eb4220e5276170f1ef9133ccdb1baee9aa28
Gernot Pointner <gernot.pointner@googlemail.com>
2019-10-24 10:04:10 +0200

    Add simple test for analysis

--- test/repo_analyzer/core_test.clj
+++ test/repo_analyzer/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [repo-analyzer.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-analyze-repository
+  (testing "Tests the analyze-repository function using the project's repository"
+    (let [result (analyze-repository ".")]
+      (is (> (count (:logs result)) 0)))))
