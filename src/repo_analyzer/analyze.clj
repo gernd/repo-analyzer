d3d978932eff79013bf8e82ab61f53cf259de8e6
Gernot Pointner <gernot.pointner@googlemail.com>
2019-10-24 21:17:39 +0200

    Bugfix for committer analysis

--- src/repo_analyzer/analyze.clj
+++ src/repo_analyzer/analyze.clj
@@ -15,10 +15,10 @@
 (defn get-logs-by-committer
   "Creates a map committer -> commits from the given logs"
   [logs]
-  (reduce #(let [comitter-name (:name (:committer %2))]
-             (if (contains? %1 comitter-name)
-               (assoc %1 comitter-name (cons (get %1 comitter-name) %2))
-               (assoc %1 comitter-name (list %2))))
+  (reduce #(let [committer-name (:name (:committer %2))]
+             (if (contains? %1 committer-name)
+               (assoc %1 committer-name (conj (get %1 committer-name) %2))
+               (assoc %1 committer-name (list %2))))
           {}
           logs))
 
