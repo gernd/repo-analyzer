9c3193e1d2da56b6e8c2b83bbe550b740969c86f
Gernot Pointner <gernot.pointner@googlemail.com>
2019-10-24 15:14:02 +0200

    Add logs by committer

--- src/repo_analyzer/analyze.clj
+++ src/repo_analyzer/analyze.clj
@@ -12,10 +12,21 @@
           {}
           logs))
 
+(defn get-logs-by-committer
+  "Creates a map committer -> commits from the given logs"
+  [logs]
+  (reduce #(let [comitter-name (:name (:committer %2))]
+             (if (contains? %1 comitter-name)
+               (assoc %1 comitter-name (cons (get %1 comitter-name) %2))
+               (assoc %1 comitter-name (list %2))))
+          {}
+          logs))
+
 (defn analyze-repository
   "Analyzes the given GIT repository and returns the analysis"
   [path-to-repo]
   (with-repo path-to-repo
              (let [logs (git-log repo)]
-               {:logs      logs
-                :by-author (get-logs-by-author logs)})))
+               {:logs         logs
+                :by-author    (get-logs-by-author logs)
+                :by-committer (get-logs-by-committer logs)})))
