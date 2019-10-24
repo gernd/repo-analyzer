7681b8c4dfc2dc1d3bacbeeadda90614871db645
Gernot Pointner <gernot.pointner@googlemail.com>
2019-10-24 09:56:52 +0200

    Separate analysis and rendering

--- src/repo_analyzer/core.clj
+++ src/repo_analyzer/core.clj
@@ -5,14 +5,20 @@
 (use 'clojure.pprint)
 
 (defn analyze-repository
-  "Analyzes the given GIT repository"
+  "Analyzes the given GIT repository and returns the analysis"
   [path-to-repo]
   (with-repo path-to-repo
-             (pprint (git-log repo))))
+             {:logs (git-log repo)}))
+
+(defn render-analysis
+  "Renders the given analysis"
+  [repo-analysis]
+  (pprint repo-analysis))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [path-to-repo]
   (println "Analyzing" path-to-repo)
-  (analyze-repository path-to-repo))
+  (let [analysis (analyze-repository path-to-repo)]
+    (render-analysis analysis)))
 
