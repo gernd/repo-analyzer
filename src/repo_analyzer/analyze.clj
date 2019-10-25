de0f815a219c3f090c9b8f4dfceb4365ed87ae57
Gernot Pointner <gernot.pointner@googlemail.com>
2019-10-25 07:03:49 +0200

    Add information about contributors

--- src/repo_analyzer/analyze.clj
+++ src/repo_analyzer/analyze.clj
@@ -1,6 +1,16 @@
 (ns repo-analyzer.analyze)
 
 (use 'clj-jgit.porcelain)
+(use 'clojure.tools.trace)
+
+(defn get-contributors
+  "Creates a mapping contributor name -> contributor info from the provided logs"
+  [logs]
+  (let [contributor-list (flatten (map #(list (:author %) (:committer %)) logs))]
+    ;(trace contributor-list)
+    (reduce #(let [name (:name %2) email (:email %2)]
+               (assoc %1 name {:name name :email email}))              ; TODO: names with different emails or names with same emails?
+            {} contributor-list)))
 
 (defn get-logs-by-author
   "Creates a map author -> commits from the given logs"
@@ -28,5 +38,6 @@
   (with-repo path-to-repo
              (let [logs (git-log repo)]
                {:logs         logs
+                :contributors (get-contributors logs)
                 :by-author    (get-logs-by-author logs)
                 :by-committer (get-logs-by-committer logs)})))
--- src/repo_analyzer/render.clj
+++ src/repo_analyzer/render.clj
@@ -1,14 +1,15 @@
 (ns repo-analyzer.render
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string])
+  (:import (java.security MessageDigest)))
 
 (use 'clojure.pprint)
 (use 'clojure.tools.trace)
 
 
 (defn create-commits-by-author-list-html
-          [author by-author-map]
-          (let [author-commit-list (get by-author-map author)]
-            (string/join (map #(string/join ["<li>" (:msg %) "</li>"]) author-commit-list))))
+  [author by-author-map]
+  (let [author-commit-list (get by-author-map author)]
+    (string/join (map #(string/join ["<li>" (:msg %) "</li>"]) author-commit-list))))
 
 (defn create-commits-by-committer-list-html
   [committer by-committer-map]
@@ -34,23 +35,57 @@
                                      "<ul>" (create-commits-by-committer-list-html % by-committer-map) "</ul>"])
                       committer-names))))
 
+(defn md5 [s]
+  (let [algorithm (MessageDigest/getInstance "MD5")
+        raw (.digest algorithm (.getBytes s))]
+    (format "%032x" (BigInteger. 1 raw))))
+
+(deftrace create-gravatar-html
+          [email]
+          (string/join
+            [
+             "<img src=\"https://www.gravatar.com/avatar/"
+             (-> email
+                 string/trim
+                 string/lower-case
+                 md5)
+             "\" />"
+             ]))
+
+
+(deftrace create-contributors-html
+          [analysis]
+          (let [contributor-list (:contributors analysis)
+                contributor-names (keys contributor-list)
+                ]
+            (string/join
+              (map #(
+                      string/join ["<p>"
+                                   %
+                                   " Email: " (:email (get contributor-list %))
+                                   " Gravatar: " (create-gravatar-html (:email (get contributor-list %)))
+                                   "</p>"]) contributor-names))))
+
 (defn create-analysis-html
   "Creates HTML for a given analysis"
   [analysis]
   (let [commit-list-html (string/join (map #(string/join ["<li>" (:msg %) "</li>"]) (:logs analysis)))
         commits-by-author-html (create-commits-by-author-html analysis)
         commits-by-committer-html (create-commits-by-committer-html analysis)
+        contributors-html (create-contributors-html analysis)
         ]
     (string/join
       ["<html><head><title>Repository Analysis></title></head>"
        "<body>"
-       "<h1>Analysis of the repository</h1>"
+       "<h1>Commit analysis</h1>"
        "<h2>List of all commits</h2>"
        "<ul>" commit-list-html "</ul>"
        "<h2>List commits by author</h2>"
        commits-by-author-html
        "<h2>List commits by committer</h2>"
        commits-by-committer-html
+       "<h1>Contributors</h1>"
+       contributors-html
        "</body></html>"])))
 
 (defn render-analysis-html
