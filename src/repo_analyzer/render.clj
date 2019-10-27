9a34cbac7976f97ae7d09c81d19a29d4df064ec8
Gernot Pointner <gernot.pointner@googlemail.com>
2019-10-27 18:14:24 +0100

    Create subpages for commit analysis lists

--- src/repo_analyzer/render.clj
+++ src/repo_analyzer/render.clj
@@ -25,6 +25,30 @@
      "\" />"
      ]))
 
+(defn create-site
+  "Creates a HTML site with basic header / footer and the given content and saves it as a file"
+  [filename site-title html-content]
+  (let [
+        site-html (string/join
+                    ["<html>"
+                     "<head>"
+                     "<title>" site-title "</title>"
+                     "<meta charset=\"utf-8\">"
+                     "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">"
+                     "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">"
+                     "</head>"
+                     "<body>"
+                     html-content
+                     ; TODO include JS if needed
+                     ;<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                     ;<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                     ;<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                     "</body></html>"])
+        ]
+    (println "Creating " filename)
+    (spit filename site-html))
+  )
+
 (defn create-commit-list-html
   "Creates HTML for a list of commits"
   [list-of-commits]
@@ -61,32 +85,30 @@
        ]
       )))
 
-
-(defn create-commit-statistics-html
-  "Creates HTML for commit statistics"
-  [analysis]
+(defn create-commit-statistics
+  "Creates HTML for commit statistics creates subpages"
+  [analysis base-path]
   (let [commit-list-html (string/join (map #(string/join ["<li>" (:msg %) "</li>"]) (:commits (:commit-statistics analysis))))
         self-commit-list-html (string/join (map #(string/join ["<li>" (:msg %) " by " (:name (:author %)) "</li>"]) (:self-committed (:commit-statistics analysis))))
         different-committer-list-html (string/join (map #(string/join ["<li>" (:msg %) " authored by " (:name (:author %))
                                                                        " committed by " (:name (:committer %))
                                                                        "</li>"]) (:committed-by-different-dev (:commit-statistics analysis))))
+        all-commits-filename (string/join [base-path "all-commits-list.html"])
+        self-commits-filename (string/join [base-path "self-committed-list.html"])
+        different-committer-filename (string/join [base-path "different-committer-list.html"])
         ]
+    (create-site all-commits-filename "All commits" commit-list-html)
+    (create-site self-commits-filename "Self committed commits" self-commit-list-html)
+    (create-site different-committer-filename "Commits where committer and author are different" different-committer-list-html)
     (string/join
       [
        "<h1>Commit analysis</h1>"
        "<p>Total number of commits: " (:number-of-commits (:commit-statistics analysis)) "</p>"
-       "<h2>List of all commits</h2>"
-       "<ul>"
-       commit-list-html
-       "</ul>"
+       "<p><a href=\"all-commits-list.html\">List of all commits</a></p>"
        "<h2>Self-committed</h2>"
-       "<ul>"
-       self-commit-list-html
-       "</ul>"
+       "<p><a href=\"self-committed-list.html\">List of all self committed commits</a></p>"
        "<h2>Committer and author are different</h2>"
-       "<ul>"
-       different-committer-list-html
-       "</ul>"
+       "<p><a href=\"different-committer-list.html\">List of all commits where author and committer differ</a></p>"
        ])
     ))
 
@@ -102,36 +124,21 @@
     ))
 
 (defn create-analysis-html-report
-  "Creates the index site for the given analysis including all subpages and returns the HTML of the index site"
+  "Creates the index site for the given analysis including all subpages"
   [analysis base-path]
-  (let [commit-statistics-html (create-commit-statistics-html analysis)
+  (let [commit-statistics-html (create-commit-statistics analysis base-path)
         contributors-html (create-contributors-html analysis)
         meta-data-html (create-meta-data-html analysis)
         index-site-html (string/join
-                          ["<html>"
-                           "<head>"
-                           "<title>Repository Analysis</title>"
-                           "<meta charset=\"utf-8\">"
-                           "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">"
-                           "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">"
-                           "</head>"
-                           "<body>"
-                           meta-data-html
+                          [meta-data-html
                            commit-statistics-html
-                           contributors-html
-                           ; TODO include JS if needed
-                           ;<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-                           ;<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-                           ;<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-                           "</body></html>"])
+                           contributors-html])
         index-site-name (string/join [base-path "index.html"])
         ]
-    (println "Creating " index-site-name)
-    (spit index-site-name index-site-html)
-    ))
+    (create-site index-site-name "Git repo analysis" index-site-html)))
 
 (defn render-analysis-html
-  "Renders the repository analysis as HTML and saves it as a file"
+  "Renders the repository analysis as HTML"
   [repo-analysis]
   (let [html-output-folder "/tmp/repo-analyzer-html/"]
     (println "Generating HTML report")
